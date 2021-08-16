e7b3aa472263c22583fc3102ad39cc2e31d33e83
aguspray001 <aguspray001@gmail.com>
2021-08-16 16:14:51 +0700

    coco ssd model detection

---
+++ utils/simpleDrawRect.js
@@ -0,0 +1,18 @@
+export const simpleDrawRect = (detections, ctx) => {
+    detections.forEach(pred => {
+        const [x, y, width, height] = pred['bbox'];
+        const text = pred['class']
+
+        // styiling
+        const color = '#' + Math.floor(Math.random()*167777215).toString(16)
+        ctx.strokeStyle = color;
+        ctx.font = '18px Arial';
+        ctx.fillStyle = color;
+
+        // draw rectangle
+        ctx.beginPath()
+        ctx.fillText(text, x, y)
+        ctx.rect(x, y, width, height)
+        ctx.stroke()
+    });
+}
